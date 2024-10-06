0be9b55a58d18132e933026b0c6ffac96e367a12
Iuliia Fokina <iuliia.fokina@gmail.com>
2024-10-06 13:42:05 -0400

    On branch main Changes to be committed: new file:   docker_build.sh

---
+++ docker_build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+./BIOS611PR/Container.sh 
+docker build . -t 611/BIOS611PR
+#check if the display variable is set
+if [ -z "$DISPLAY" ]; then
+echo "error: display environment variable is not set"
+exit 1
+fi
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to run the first script."
+    exit 1
+fi
+echo "Starting container."
+echo "Open localhost:8787 to view RStudio. Username: rstudio, password: login"
+echo "Files found under '/home/rstudio/working'."
+#get the current directory
+CURRENT_DIR=$(pwd)
+CONT=$(docker run --rm -tidp 8787:8787 -v .:/home/rstudio/working alectries/univr)  # when creating a new Dockerfile, change to .
+docker exec -ti $CONT /bin/bash
+
+
